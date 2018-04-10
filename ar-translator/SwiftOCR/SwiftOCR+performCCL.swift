91cb4dc00cbde23a89b304f0f51c24b31a9b86d8
mikolajspeichert <mikolaj.speichert@gmail.com>
2018-04-10 22:56:51 +0200

    Add SwiftOCR as a internal module

---
+++ ar-translator/SwiftOCR/SwiftOCR+performCCL.swift
@@ -0,0 +1,21 @@
+
+public extension SwiftOCR {
+    /**
+     
+     Extracts the characters using [Connected-component labeling](https://en.wikipedia.org/wiki/Connected-component_labeling) and callbacks with their location data
+     
+     - Parameter image:             The image used for OCR
+     - Parameter completionHandler: The completion handler that gets invoked after CCL is finished, returning
+     results of this action.
+     
+     */
+    open func performCCL(_ image: OCRImage, _ onCompleted: [(Float, Float)] -> Void){
+        DispatchQueue.global(qos: .userInitiated).async {
+            let preprocessedImage      = self.delegate?.preprocessImageForOCR(image) ?? self.preprocessImageForOCR(image)
+            
+            let blobs                  = self.extractBlobs(preprocessedImage)
+            // extract location
+            onCompleted(blobs)
+        }
+    }
+}
